7f90e7a570c31b67574a3e6ef3628ab594abbd95
Anh Ng <rean1luv.nta1510@gmail.com>
2024-05-06 23:51:15 +0700

    add sql

--- Create Schema.sql
+++ Create Schema.sql
@@ -1,2 +1,19 @@
-CREATE SCHEMA IF NOT EXISTS minio.sales
+CREATE SCHEMA IF NOT EXISTS minio.my_schema
 WITH (location = 's3a://sales/');
+
+
+CREATE TABLE IF NOT EXISTS minio.my_schema.my_table (
+    id INT,
+    name VARCHAR,
+    age INT
+) WITH (
+    format = 'Parquet',
+    external_location = 's3a://trino/'
+);
+
+
+INSERT INTO minio.my_schema.my_table
+VALUES 
+    (4, 'John', 30),
+    (5, 'Alice', 25),
+    (6, 'Bob', 35);
