97e0013b7a52f081bfa2eb32da15d023d5a68324
Anh Ng <rean1luv.nta1510@gmail.com>
2024-05-10 00:24:11 +0700

    update

--- Create Schema.sql
+++ Create Schema.sql
@@ -17,3 +17,22 @@ VALUES
     (4, 'John', 30),
     (5, 'Alice', 25),
     (6, 'Bob', 35);
+
+
+
+
+   
+ CREATE  TABLE IF NOT EXISTS minio.my_schema.ext_tiki_products (
+    tiki_pid BIGINT,
+    name VARCHAR,
+    brand_name VARCHAR,
+    origin VARCHAR,
+    ingestion_date DATE,
+    ingestion_dt_unix BIGINT
+) WITH (
+    format = 'Parquet',
+    external_location = 's3a://dim-product/'
+)
+
+# Tại sao sử dụng hive nhưng tại không có type string
+# Tại sao trino không có external table
