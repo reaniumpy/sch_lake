61d1325d28aebc08abf1f4877e44d2e2c90629b2
theanh <theanhnguyen@ssis.edu.vn>
2024-06-06 14:34:06 +0700

    update compose & jvm config

--- iceberg_iris.sql
+++ iceberg_iris.sql
@@ -1,6 +1,8 @@
+-- Create database schema
 CREATE SCHEMA IF NOT EXISTS iceberg.iris
     WITH (location = 's3a://iris/');
 
+-- Create table
 CREATE TABLE IF NOT EXISTS iceberg.iris.iris_parquet
 (
     sepal_length DOUBLE,
@@ -9,44 +11,26 @@ CREATE TABLE IF NOT EXISTS iceberg.iris.iris_parquet
     petal_width  DOUBLE,
     class        VARCHAR
 )
-WITH (format = 'PARQUET',location = 's3a://iris/vv/');
+WITH (format = 'PARQUET',location = 's3a://iris/v1/');
 
 
-
-
-CREATE SCHEMA IF NOT EXISTS hive.iris
-    WITH (location = 's3a://iris/');
-
-CREATE TABLE IF NOT EXISTS hive.iris.iris_parquet
-(
-    sepal_length DOUBLE,
-    sepal_width  DOUBLE,
-    petal_length DOUBLE,
-    petal_width  DOUBLE,
-    class        VARCHAR
-)
-WITH ( location = 's3a://iris/',
-    format = 'PARQUET');
-
-
-
-
-   
-   INSERT INTO iceberg.iris.iris_parquet
+-- Insert new data
+INSERT INTO iceberg.iris.iris_parquet
 VALUES
     (6, 3.5, 1.4, 0.2, 'Yeah')
     
     
-    
-    
+
+-- Update 
 UPDATE
   iceberg.iris.iris_parquet
 SET
-  class = 'zz'
+  class = 'Yeah'
 where sepal_length = 5.1
   ship_date IS NULL;
 
 
+-- Time travel
 SELECT * 
 FROM iceberg.iris.iris_parquet
-FOR TIMESTAMP AS OF TIMESTAMP '2022-03-23 09:59:29'
+FOR TIMESTAMP AS OF TIMESTAMP '2022-03-23 09:59:29' -- Change the time
